385c4eb40e0b8ad1b0b555ad12ad78a42c69da81
Juan Costa <juandev1998@gmail.com>
2024-09-15 15:08:35 -0300

    H2 database, test profile, JPA

---
+++ main/java/com/larvastore/demo/resourcers/UserResource.java
@@ -0,0 +1,26 @@
+package com.larvastore.demo.resourcers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.larvastore.demo.entidades.Register;
+import com.larvastore.demo.entidades.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "juan", "teste@gmail.com", "27963451", "12345678");
+		
+		return ResponseEntity.ok().body(u);
+	}
+
+	
+}
