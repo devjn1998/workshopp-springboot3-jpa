2da50ad0db0d41a96750605a95e89542bf8d6c19
Juan Costa <juandev1998@gmail.com>
2024-09-20 22:24:32 -0300

    Entity Products

--- main/java/com/larvastore/demo/config/TestConfig.java
+++ main/java/com/larvastore/demo/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.larvastore.demo.entidades.Category;
 import com.larvastore.demo.entidades.Order;
+import com.larvastore.demo.entidades.Product;
 import com.larvastore.demo.entidades.User;
 import com.larvastore.demo.entidades.enums.OrderStatus;
 import com.larvastore.demo.repositories.CategoryRepository;
 import com.larvastore.demo.repositories.OrderRepository;
+import com.larvastore.demo.repositories.ProductRepository;
 import com.larvastore.demo.repositories.UserRepository;
 
 @Configuration
@@ -27,6 +29,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -34,6 +39,12 @@ public class TestConfig implements CommandLineRunner {
 		Category cat1 = new Category(null, "Eletronicos");
 		Category cat2 = new Category(null, "Livros");
 		Category cat3 = new Category(null, "Esportes");
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
 		
 
@@ -48,5 +59,6 @@ public class TestConfig implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 	}
 }
